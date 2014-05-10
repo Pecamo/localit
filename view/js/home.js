f6f51bc1631fc8502e59b148ff735710fb4d6ad4
Flagoul <basile.vu@gmail.com>
2014-05-10 18:45:39 +0200

    Beginning function to display a post

--- view/js/home.js
+++ view/js/home.js
@@ -1,1 +1,9 @@
-$('#posts_container').append('<div class="post">Test<div>')
+$(function () {
+	for(var i = 0; i < 3; ++i) {
+		display_post();
+	}
+})
+
+function display_post() {
+	$('#posts_container').append('<div class="post">Post<div>')
+}
