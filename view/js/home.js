e969d2fd34f6bf7b7186266fb4bc2ec25bd358ae
Flagoul <basile.vu@gmail.com>
2014-05-10 22:33:44 +0200

    Updated display_post

--- view/js/home.js
+++ view/js/home.js
@@ -2,31 +2,30 @@ $(function () {
 	
 	for(var i = 0; i < 3; ++i) {
 		var post = new Array()
-		post = {"id": i, "ups": 0, "title": "Post title", "content": "Post content !", "open": false}
+		post = {"id": i, "ups": 0, "title": "Post title", "content": "Post content", "open": false}
 		display_post(post);
 	}
 })
 
 function display_post(post) {
-	$('#posts_container').append(
-		'<div id="post'+ post.id +'" class="row">' +
-			'<div class="col-md-1 post_up">'+ post.ups +'</div>' +
-			'<div class="post_title">'+ 
-				'<div class="panel-group" id="accordion">' +
-				  '<div class="panel panel-default">' +
-					'<div class="panel-heading">' +
-					  '<h4 class="panel-title">' +
-						'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+ post.id +'">' + post.title + '</a>' +
-					  '</h4>' +
-					'</div>' +
-					'<div id="collapse'+ post.id +'" class="panel-collapse collapse">' +
-					  '<div class="panel-body">' +
-						'Marrant contenu' +
-					  '</div>' +
-					'</div>' +
-				  '</div>' +
+	var s = 
+	'<div id="post'+post.id+'" class="post">' +
+		'<div class="panel panel-default">' +
+			'<div class="panel-heading">' +
+				'<div>' + post.ups + '</div>' +
+				'<h4 class="panel-title">' +
+					'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+post.id+'">' +
+							post.title +
+					'</a>' +
+				'</h4>' +
+			'</div>' +
+			'<div id="collapse'+post.id+'" class="panel-collapse collapse">' +
+				'<div class="panel-body">' +
+					post.content +
 				'</div>' +
 			'</div>' +
-		'</div>'
-	)
+		'</div>' +
+	'</div>'
+
+	$('.posts_container').append(s);
 }
