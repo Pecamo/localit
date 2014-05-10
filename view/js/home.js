2fbf69532ce2c5d00a07f9b2db2d85ac337260bc
Flagoul <basile.vu@gmail.com>
2014-05-11 00:24:51 +0200

    Working on the posts header

--- view/js/home.js
+++ view/js/home.js
@@ -2,7 +2,7 @@ $(function () {
 	
 	for(var i = 0; i < 3; ++i) {
 		var post = new Array()
-		post = {"id": i, "ups": 0, "title": "Post title", "content": "Post content"}
+		post = {"id": i, "ups": 0, "title": "Post title", "content": "Post content", "location": "EPFL"}
 		display_post(post);
 	}
 })
@@ -11,15 +11,22 @@ function display_post(post) {
 	var s = 
 		'<div id="post'+post.id+'" class="panel panel-default post">' +
 			'<div class="panel-heading">' +
-				'<div class="pull-left ups">' + 
-					post.ups + 
-				'</div>' +
-				'<div>' +
-					'<h4 class="panel-title">' +
-						'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+post.id+'">' +
-								post.title +
-						'</a>' +
-					'</h4>' +
+				'<div class="post_header">' +
+					'<div class="ups pull-left">' + 
+						'<div onclick="upvote()" class="glyphicon glyphicon-arrow-up"></div>' +
+						'<div>' + post.ups + '</div>' +
+					'</div>' +
+					'<div class="post_first_line">' +
+						'<div class="panel-title">' +
+							'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+post.id+'">' +
+									post.title +
+							'</a>' +
+						'</div>' +
+						'<div> at '+ post.location + '</div>' +
+					'</div>' +
+					'<div class="post_second_line">' +
+						
+					'</div>' +
 				'</div>' +
 				
 			'</div>' +
@@ -31,4 +38,8 @@ function display_post(post) {
 		'</div>'
 
 	$('.posts_container').append(s);
+}
+
+function upvote() {
+	alert("Fnupvoted !")
 }
