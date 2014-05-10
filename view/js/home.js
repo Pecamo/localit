8b64c1631352b4781cd590754d803ad7525ba135
Flagoul <basile.vu@gmail.com>
2014-05-10 23:54:37 +0200

    Push d'une manière brève
    Not that much changes

--- view/js/home.js
+++ view/js/home.js
@@ -2,7 +2,7 @@ $(function () {
 	
 	for(var i = 0; i < 3; ++i) {
 		var post = new Array()
-		post = {"id": i, "ups": 0, "title": "Post title", "content": "Post content", "open": false}
+		post = {"id": i, "ups": 0, "title": "Post title", "content": "Post content"}
 		display_post(post);
 	}
 })
@@ -11,12 +11,17 @@ function display_post(post) {
 	var s = 
 		'<div id="post'+post.id+'" class="panel panel-default post">' +
 			'<div class="panel-heading">' +
-				'<div>' + post.ups + '</div>' +
-				'<h4 class="panel-title">' +
-					'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+post.id+'">' +
-							post.title +
-					'</a>' +
-				'</h4>' +
+				'<div class="pull-left ups">' + 
+					post.ups + 
+				'</div>' +
+				'<div>' +
+					'<h4 class="panel-title">' +
+						'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+post.id+'">' +
+								post.title +
+						'</a>' +
+					'</h4>' +
+				'</div>' +
+				
 			'</div>' +
 			'<div id="collapse'+post.id+'" class="panel-collapse collapse">' +
 				'<div class="panel-body">' +
