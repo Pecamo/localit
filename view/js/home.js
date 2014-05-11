b5d6dd71ba35e7a08cd48f17fdaf003776eb9114
Flagoul <basile.vu@gmail.com>
2014-05-11 02:36:08 +0200

    Adding delete links if user is the author

--- view/js/home.js
+++ view/js/home.js
@@ -9,8 +9,14 @@ $(function () {
 
 function display_post(post) {
 	// todo
+	var currentUser = "The pouldre"
 	var locDiff = "0 km"
 	var lastPosted = "0 s"
+	
+	var del = ""
+	if(post.author == currentUser) {
+		del = '<div id="delete_link' + post.id + '" class="pull-right small_text"></div>'
+	}
 	
 	var s = 
 		'<div id="post' + post.id + '" class="panel panel-default post">' +
@@ -29,8 +35,8 @@ function display_post(post) {
 					'<span class="post_second_line small_text">' +
 						'by ' + post.author + " - " + lastPosted + " ago" +
 					'</span>' +
+					del +
 				'</div>' +
-				
 			'</div>' +
 			'<div id="collapse' + post.id + '" class="panel-collapse collapse">' +
 				'<div class="panel-body">' +
@@ -40,9 +46,22 @@ function display_post(post) {
 		'</div>'
 
 	$('.posts_container').append(s);
+	$('#delete_link' + post.id).append("delete this post")
+	$('#delete_link' + post.id).on('click', function () {
+		if(post.author == currentUser) {
+			deletePost(post)
+		}
+		else {
+			alert("You are not allowed to do this.")
+		}
+	})
 }
 
 // todo
 function upvote() {
 	alert("Fnupvoted !")
+}
+
+function deletePost(post) {
+	alert("You deleted this post.")
 }
