506fd78f5da6df791f9ab26cfa42e6f92b8a5740
Solal Pirelli <solal.pirelli@gmail.com>
2014-05-11 11:59:48 +0200

    Chtuff. [mode Lenstra off]

--- Server/Localit.Server/Controllers/PostsController.cs
+++ Server/Localit.Server/Controllers/PostsController.cs
@@ -20,50 +20,24 @@ namespace Localit.Server.Controllers
 
         [HttpGet]
         [Route( "" )]
-        public IEnumerable<Post> GetPostsNear( [FromUri] double latitude, [FromUri] double longitude, [FromUri( Name = "range" )] double? optionalRange = null, [FromUri( Name = "userFacebookId" )] long? userFbId = null )
+        public IEnumerable<Post> GetPostsNear( [FromUri] double latitude, [FromUri] double longitude, [FromUri] double range, [FromUri] long? userFacebookId = null )
         {
-            IQueryable<Post> results;
+            var results = ( from p in _context.Posts
+                            let dlon = p.Location.Longitude - longitude // haversine
+                            let dlat = p.Location.Latitude - latitude
+                            let a = SqlFunctions.Square( SqlFunctions.Sin( dlat / 2 ) ) + SqlFunctions.Cos( latitude ) * SqlFunctions.Cos( p.Location.Latitude ) * SqlFunctions.Square( SqlFunctions.Sin( dlon / 2 ) )
+                            let c = 2 * SqlFunctions.Atan2( SqlFunctions.SquareRoot( a ), SqlFunctions.SquareRoot( 1 - a ) )
+                            let dist = c * EarthRadius
+                            where dist <= range
+                            orderby p.Score descending
+                            select p ).Include( p => p.Creator );
 
-            var stuff = from p in _context.Posts
-                        let dlon = p.Location.Longitude - longitude
-                        let dlat = p.Location.Latitude - latitude
-                        let a = SqlFunctions.Square( SqlFunctions.Sin( dlat / 2 ) ) + SqlFunctions.Cos( latitude ) * SqlFunctions.Cos( p.Location.Latitude ) * SqlFunctions.Square( SqlFunctions.Sin( dlon / 2 ) )
-                        let c = 2 * SqlFunctions.Atan2( SqlFunctions.SquareRoot( a ), SqlFunctions.SquareRoot( 1 - a ) )
-                        where c != null
-                        let distance = c * EarthRadius
-                        select new { Distance = (double) distance, Post = p };
-
-            if ( optionalRange.HasValue )
-            {
-                double range = optionalRange.Value;
-
-                results = ( from o in stuff
-                            where o.Distance <= 1.25 * range
-                            orderby new[] { (double) SqlFunctions.Cos( ( ( o.Distance - 50 ) * Math.PI / 2 ) / ( range / 4.0 ) ) * 0.5, 1 - ( 0.5 / range ) * o.Distance }.Min() * o.Post.Score descending
-                            select o.Post ).Include( p => p.Creator );
-            }
-            else
-            {
-                var avgx = from o in stuff
-                           where o.Distance <= 10
-                           where o.Post.Score != null
-                           select o.Post.Score;
-
-
-                double avg = avgx.Any() ? avgx.Average() : 0;
-
-                results = ( from o in stuff
-                            let popFact = new[] { ( o.Post.Score / 2.0 * avg ) * 0.5, 0.5 }.Min()
-                            orderby SqlFunctions.Cos( o.Distance * Math.PI / 10 ) * ( 1 - popFact ) + popFact descending
-                            select o.Post ).Include( p => p.Creator );
-            }
-
-            if ( userFbId == null )
+            if ( userFacebookId == null )
             {
                 return results;
             }
 
-            var user = _context.Users.FirstOrDefault( u => u.FacebookId == userFbId );
+            var user = _context.Users.FirstOrDefault( u => u.FacebookId == userFacebookId );
             if ( user == null )
             {
                 throw new HttpResponseException( HttpStatusCode.BadRequest );
